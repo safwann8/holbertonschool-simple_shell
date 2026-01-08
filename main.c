e162691401544fe465f3cf8b1213d2de6f098f5d
safwann8 <safoulefou83@gmail.com>
2026-01-08 17:34:56 +0100

    Add initial implementation of simple shell with AUTHORS and README files

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+
+/**
+ * main - Simple UNIX command line interpreter
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	pid_t pid;
+	int status;
+
+	while (1)
+	{
+		write(STDOUT_FILENO, "#cisfun$ ", 9);
+
+		if (getline(&line, &len, stdin) == -1)
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			free(line);
+			return (0);
+		}
+
+		line[strcspn(line, "\n")] = '\0';
+
+		pid = fork();
+		if (pid == -1)
+			return (1);
+
+		if (pid == 0)
+		{
+			char *argv[] = {line, NULL};
+
+			execve(line, argv, environ);
+			perror("./shell");
+			exit(1);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+}
