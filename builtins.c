a1e0611482ade301fe8375e68608ea183eaf0026
safwann8 <safoulefou83@gmail.com>
2026-01-14 00:28:17 +0000

    add env builtin to print environment variables

--- builtins.c
+++ builtins.c
@@ -9,10 +9,26 @@
  */
 int handle_builtins(char **argv, char *line)
 {
+	int i = 0;
+
 	if (strcmp(argv[0], "exit") == 0)
 	{
 		free(line);
 		exit(last_status);
 	}
+
+	if (strcmp(argv[0], "env") == 0)
+	{
+		while (environ[i])
+		{
+			write(STDOUT_FILENO, environ[i],
+			      strlen(environ[i]));
+			write(STDOUT_FILENO, "\n", 1);
+			i++;
+		}
+		last_status = 0;
+		return (1);
+	}
+
 	return (0);
 }
